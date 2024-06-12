967dea812d99f9c07a1d8b9ea68c3c36c73b8aca
Aksharpatel06 <aksharpatel030306@gmail.com>
2024-06-12 10:54:17 +0530

    12-06-2024

--- lib/utils/recent_listning_list.dart
+++ lib/utils/recent_listning_list.dart
@@ -1,22 +1,22 @@
 List recentListningList =[
   {
     'img':'asset/img/recent listen/img1.png',
-    'name':'Harry’s House',
-    'song': 'asset/audio/song/music16.mp3',
+    'name':'Teri Baatan Mein Aisa Uljha Jiya',
+    'song': 'asset/audio/song/music19.mp3',
   },
   {
     'img':'asset/img/recent listen/img2.png',
-    'name':'current favorites',
-    'song': 'asset/audio/song/music17.mp3',
+    'name':'Bhediya',
+    'song': 'asset/audio/song/music20.mp3',
   },
   {
     'img':'asset/img/recent listen/img3.png',
-    'name':'3:00am vibes',
-    'song': 'asset/audio/song/music18.mp3',
+    'name':'flying jatt',
+    'song': 'asset/audio/song/music21.mp3',
   },
   {
     'img':'asset/img/recent listen/img4.png',
-    'name':'rain on my window',
-    'song': 'asset/audio/song/music19.mp3',
+    'name':'Kalank',
+    'song': 'asset/audio/song/music22.mp3',
   },
 ];
